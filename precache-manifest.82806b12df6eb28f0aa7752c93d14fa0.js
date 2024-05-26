d38ecc9df222a745622333ec8f1ef2fbf17cf3ac
esrabelhassenn <belhassennesra@gmail.com>
2024-05-26 22:34:01 +0100

    Updates

---
+++ precache-manifest.82806b12df6eb28f0aa7752c93d14fa0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e298608c876fce60c4bbda70f5770eb5",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "47172211e3f627deb67b",
+    "url": "/movie/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "c986963fe1773ce3194b",
+    "url": "/movie/static/css/main.6544d106.chunk.css"
+  },
+  {
+    "revision": "47172211e3f627deb67b",
+    "url": "/movie/static/js/2.8bf9a618.chunk.js"
+  },
+  {
+    "revision": "f00d1589be2d048e82fe085469454435",
+    "url": "/movie/static/js/2.8bf9a618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c986963fe1773ce3194b",
+    "url": "/movie/static/js/main.789c49bd.chunk.js"
+  },
+  {
+    "revision": "e1dd6779ca1aee5d74c5",
+    "url": "/movie/static/js/runtime-main.bdbebd6e.js"
+  },
+  {
+    "revision": "d8a5b388d3acb4ad95b32ea7efbca536",
+    "url": "/movie/static/media/assortment-cinema-elements-red-background-with-copy-space.d8a5b388.jpg"
+  }
+]);
